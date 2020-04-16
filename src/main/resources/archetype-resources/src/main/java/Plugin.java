127b0edb6d5e2f6e9e25eb5e045e0c5db806a452
LeftPathLane <LeftPathLane@github.com>
2020-04-16 03:01:49 +0000

    1.15

---
+++ src/main/resources/archetype-resources/src/main/java/Plugin.java
@@ -0,0 +1,10 @@
+package $package;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/PluginTest.java
@@ -0,0 +1,12 @@
+package $package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PluginTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
